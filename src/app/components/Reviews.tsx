846495bf4c8310fc80aaa2ed0400fe5188525617
4pillsAday <arafat0324@gmail.com>
2025-09-12 12:46:55 -0700

    User review section improvement #2

--- src/app/components/Reviews.tsx
+++ src/app/components/Reviews.tsx
@@ -106,7 +106,7 @@ const Reviews = ({ diveSiteSlug }: ReviewsProps) => {
     <div className="bg-white antialiased">
       <div className="container mx-auto px-4 py-8">
         <div className="flex justify-between items-center mb-6">
-          <h2 className="text-lg font-bold">
+          <h2 className="text-lg font-bold text-black">
             Site Reviews
           </h2>
         </div>
